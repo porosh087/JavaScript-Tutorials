b0af4b8cf6c1a82d5be581c810c78d5eea53d4f5
porosh087 <122595608+porosh087@users.noreply.github.com>
2025-05-29 12:40:17 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-/*
-  *************Concept: Declaring Variables********************
-  In JavaScript, you can declare variables using var, let, and const.
-
-  * let → allows you to reassign values.
-  * const → cannot be reassigned.
-  * var → older version (we avoid using it in modern JS).
-*/
