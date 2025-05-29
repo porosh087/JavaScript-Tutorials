64296fbcf94c8e8566c120fd00a24403d971eeb7
porosh087 <rakibhossainmern@gmail.com>
2025-05-29 12:36:37 +0600

    JS Introduction Complete

---
+++ JS Introduction/script.js
@@ -0,0 +1,28 @@
+/*
+  JavaScript is a Programming Language of the web
+  It can update and change Both HTML and CSS
+  It can calculate manipulate and validate data
+*/
+
+/*
+  *************Concept: Declaring Variables********************
+  In JavaScript, you can declare variables using var, let, and const.
+
+  * let → allows you to reassign values.
+  * const → cannot be reassigned.
+  * var → older version (we avoid using it in modern JS).
+*/
+// Output
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+console.log("Hello JS Introduction");
+
+// Add CSS
+document.getElementById("demo").style.fontSize = "45px";
+document.getElementById("demo").style.color = "Blue";
+
+// Write Output
+document.write(5+6);
+
+// Alart Output
+window.alert("Welcome");
+alert("Porosh");
